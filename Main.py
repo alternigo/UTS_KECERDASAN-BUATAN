affad12c0a914a75dd5ab4f82600c68b2a2afb0c
denidhr <denidharmawan14@gmail.com>
2022-04-21 21:56:07 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,26 @@
+# Nama  : Deni Dharmawan
+# NIM   : 191011400321
+# Kelas : 06TPLM003
+
+
+#Pengulangan While Loop
+count = 0
+while (count < 9):
+    print ("The count is: ", count)
+    count = count + 1
+
+print ("Akhir dari program while loop \n")
+
+#Pengulangan for sederhana
+angka = [0,1,2,3,4]
+for x in angka:
+    print(x)
+
+print ("Akhir dari program for sederhana \n")
+
+#Pengulangan for
+buah = ["semangka", "jeruk", "anggur"]
+for makanan in buah:
+    print ("Saya suka makan", makanan)
+
+print ("Akhir dari program for")
